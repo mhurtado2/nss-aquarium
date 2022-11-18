2348ba42c4be22ebd22e792a9a4b655dd9116491
Shane <mhurtado1@berklee.edu>
2022-11-18 11:12:38 -0600

    2nd martins

---
+++ scripts/database.js
@@ -0,0 +1,48 @@
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            food: "crustaceans",
+            size: 3
+        },
+        
+        {
+            name: "Ornate Sleeper Ray",
+            food: "lobster",
+            size: 10
+        },
+   
+        {
+            name: "Devil's Hole Pupfish",
+            food: "goldfish",
+            size: 1.2
+        },
+
+        {
+            name: "American Paddlefish",
+            food: "puffer fishs",
+            size: 57
+        },
+
+        {
+            name: "Peppermint Angelfish",
+            food: "candy",
+            size: 2.8
+        },
+
+        {
+            name: "Bluefin Tuna",
+            food: "iguanas",
+            size: 59
+        },
+    
+        {
+            name: "Red Handfish",
+            food: "stuff",
+            size: 13.5
+        }
+    
+    ]
+}
+
+console.log(database)
