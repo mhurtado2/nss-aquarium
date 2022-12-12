8fe4388f3da84fad83bcad4471956f9079b5f97d
Shane <mhurtado1@berklee.edu>
2022-12-12 09:48:58 -0600

    martins-aquarium

--- scripts/database.js
+++ scripts/database.js
@@ -1,48 +1,83 @@
+
 const database = {
-    fish: [
+     fishes: [
         {
+            image: "https://mystickermania.com/cdn/stickers/simpsons-stickers/sticker_5006-512x512.png",
             name: "Bart",
-            food: "crustaceans",
-            size: 3
+            species: "Simpson's Fish",
+            length: 3,
+            location: "Spring Ocean",
+            food: "crustaceans"
         },
         
         {
-            name: "Ornate Sleeper Ray",
-            food: "lobster",
-            size: 10
+            image: "https://static.wikia.nocookie.net/animals/images/6/62/1_RAY_461.jpg",
+            name: "Ray",
+            species:"Ornate Sleeper Ray",
+            length: 10,
+            location:"South Africa", 
+            food: "lobster"
         },
    
         {
-            name: "Devil's Hole Pupfish",
-            food: "goldfish",
-            size: 1.2
+            image: "https://upload.wikimedia.org/wikipedia/commons/3/37/Cyprinodon_diabolis%2C_males.jpg",
+            name: "Lou",
+            species:"Devil's Hole Pupfish",
+            length: 1.2,
+            location:"Nevada",
+            food: "goldfish"
         },
 
         {
-            name: "American Paddlefish",
-            food: "puffer fishs",
-            size: 57
+            image: "https://upload.wikimedia.org/wikipedia/commons/6/67/Paddlefish-USFWS-Fish-and-Aquatic-Conservation-2160x1440.jpg",
+            name: "Paddy",
+            species: "American Paddlefish",
+            length: 57,
+            location: "Montana",
+            food: "puffer fish"
         },
 
         {
-            name: "Peppermint Angelfish",
-            food: "candy",
-            size: 2.8
+            image: "http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcS--jFTm2rFLDBquyYF1Dcpbp87KP4ZuFpp5RbXXtntYBEn_1zGOST6M-9CR6kqdSsaZp5lIVEyqfGPICc",
+            name: "Pep",
+            species:"Peppermint Angelfish",
+            length: 2.8,
+            location: "Cook Island",
+            food: "candy"
         },
 
         {
-            name: "Bluefin Tuna",
-            food: "iguanas",
-            size: 59
+            image: "https://fishingbooker.com/blog/media/Bluefin-Tuna-e1551478845735-1200x675.jpg",
+            name: "Blueface",
+            species:"Bluefin Tuna",
+            length: 59,
+            location: "California" ,
+            food: "iguanas"
         },
     
         {
-            name: "Red Handfish",
-            food: "stuff",
-            size: 13.5
+            image: "https://upload.wikimedia.org/wikipedia/commons/9/9a/CSIRO_ScienceImage_2535_The_Red_Handfish.jpg",
+            name: "Ol Red",
+            species:"Red Handfish",
+            length: 13.2,
+            location: "Tasmania",
+            food: "stuff"
         }
     
     ]
 }
 
+//check to see if data base worked
 console.log(database)
+/*
+ const getFish = () => {
+    return database.fishes.map(fish => ({...fish}))
+}
+*/
+export const getFish = () => {
+    return database.fishes.map(fish => ({...fish}))
+}
+
+//console.log(getFish())
+
+//module.exports = { getFish }
